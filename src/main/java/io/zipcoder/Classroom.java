2103a1505910f543271b061547d0c2497a0cef4b
Nirmala <nirmala.talari@gmail.com>
2019-03-01 21:31:36 -0500

    GradedSubmission

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -60,17 +60,44 @@ public class Classroom {
     }
 
     public void getGradeBook() {
-        Map<ArrayList<Student>, Integer> studMap = new TreeMap<>();
-        Integer count = 1;
-        for (int i = 0; i < students.size(); i++) {
-            studMap.put(this.students,students.get(i).getAverageExamScore());
+        String str = "";
+        //  Student[] studArr = new Student[students.size() + 1];
+        ArrayList<String> list = new ArrayList<>();
+        Map<Integer, String> map = new TreeMap();
+        //  studArr = Arrays.copyOf(students.toArray(studArr), students.size() + 1);
+        Integer i = 0;
+        for (Student stud : students) {
+            map.put(stud.getAverageExamScore(), stud.getLastName());
         }
+        Integer count = map.size() - 1;
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            if (((count/ (map.size())) * 100) > 10) {
+                str += entry.getValue() + " " + "Graded: A" + "\n";
+            }
+            else if (((count / (map.size())) * 100) > 11 || ((((map.size() - 1) / (map.size())) * 100) < 29)) {
+                str += entry.getValue() + " " + "Graded: B" + "\n";
+            }
+            else if (((count / (map.size())) * 100) > 30 || ((((map.size() - 1) / (map.size())) * 100) < 50)) {
+                str += entry.getValue() + " " + "Graded: C" + "\n";
+            }
+            else if (((count / (map.size())) * 100) > 51 || ((((map.size() - 1) / (map.size())) * 100) < 89)) {
+                str += entry.getValue() + " " + "Graded: D" + "\n";
+            }
+            else if (((count / (map.size())) * 100) < 11) {
+                str += entry.getValue() + " " + "Graded: F" + "\n";
+            }
+            count--;
+        }
+        System.out.println(str);
+    }
+}
+
 //        An A is awarded to students whose class average is in the upper 10th percentile.
 //        A B is awarded to students whose class average falls between the 11th and 29th percentile.
 //        A C is awarded to students whose class average falls between the 30th and 50th percentile.
 //        A D is awarded to students whose class average falls between the 51st and 89th percentile.
 //        An F is awarded to students whose class average is in the lower 11th percentile.
+    //   Percentile = (number of people behind you/total number of people) x 100
 
-    }
 
-}
